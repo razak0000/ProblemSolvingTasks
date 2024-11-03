e1fd273f9b3b91c074f41d32f66249729dda96d0
razak0000 <xquiringdreamer@gmail.com>
2024-11-03 19:22:58 +0530

    feat task8 added

--- task8.cpp
+++ task8.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int isprime(int n){
-    if(n==0 || n==1){
+#include <iostream>
+using namespace std;
+
+bool isprime(int n) {
+    if (n <= 1) {
         return false;
     }
-    for(int i=2;i<=5;i++){
-        if(n%i==0){
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
             return false;
-        }
-        else{
-            return true;
         }
     }
     return true;
