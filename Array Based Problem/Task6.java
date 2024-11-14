7ea28438dd55065679227b45ca70c6f36f53aa90
razak0000 <xquiringdreamer@gmail.com>
2024-11-14 15:27:01 +0530

    feat task added

--- Array Based Problem/Task6.java
+++ Array Based Problem/Task6.java
@@ -1,5 +1,28 @@
 package placementclass.skill;
+import java.util.Arrays;
+
 
 public class Task6 {
-
+    public static void sum6(){
+        int[] num={4,3,2,1};
+        int sum=0;
+        int n=num.length;
+        for(int i=0;i<num.length;i++){
+            int fnum=num[i];
+            sum=(sum*10)+fnum;
+        }
+        int fsum=sum+1;
+        while(fsum!=0){
+            int last=fsum%10;
+            num[n-1]=last;
+            fsum=fsum/10;
+            n--;
+    
+        }
+        System.out.println(Arrays.toString(num));
+    }
+    public static void main(String[] args) {
+		sum6();
+	}
+    
 }
